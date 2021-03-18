c6a8e3ce2745e6ed9f35a4ca8ee1c479720cdfa2
Michael-Nobrega <2044720@student.uma.pt>
2021-03-18 17:06:40 +0000

    Update index.js

--- Ficha1/index.js
+++ Ficha1/index.js
@@ -3,4 +3,147 @@ var b = 5;
 
 var c = a + b;
 
-console.log(c)
+// console.log(c);
+
+var miniProj = 16;
+var freq = 10;
+var projFinal = 12;
+
+var notaFinal = miniProj * 0.3 + freq * 0.3 + 0.3 + projFinal * 0.4;
+
+//console.log("A nota final é de " + Math.round(notaFinal) + " valores");
+
+//6. Implemente uma função que receba como argumento o número do mês e imprima o nome por extenso
+/*
+
+    var month = 3
+
+    if (month == 1) {
+        console.log("Janeiro");
+    } else if (month == 2) {
+        console.log("Fevereiro");
+    } else if (month == 3) {
+        console.log("Março");
+    } else if (month == 4) {
+        console.log("Abril");
+    } else if (month == 5) {
+        console.log("Maio");
+    } else if (month == 6) {
+        console.log("Junho");
+    } else if (month == 7) {
+        console.log("Julho");
+    }
+
+    switch (month) {
+        case 1:
+            console.log("Janeiro");
+            break;
+        case 2:
+            console.log("Fevereiro");
+            break;
+        case 3:
+            console.log("Março");
+            break;
+        case 4:
+            console.log("Abril");
+            break;
+        case 5:
+            console.log("Maio");
+            break;
+        case 6:
+            console.log("Junho");
+            break;
+        case 7:
+            console.log("Julho");
+            break;
+        case 8:
+            console.log("Agosto");
+            break;
+        case 9:
+            console.log("Setembro");
+            break;
+        case 10:
+            console.log("Outubro");
+            break;
+        case 11:
+            console.log("Novembro");
+            break;
+        case 12:
+            console.log("Dezembro");
+            break; 
+        default:
+            console.group("Mes invalido")
+            break;
+    }
+
+    var months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio"]
+
+
+    if (month - 1 > 12 || month - 1 < 0){
+        console.log("Mes Inválido")
+    }
+    else{
+        console.log(months[month - 1])
+    }  
+*/
+ // 7.Implemente uma função que receba dois números e uma operação ( + , - , * , / ou ^) e imprima o resultado da operação sobre os números.
+
+ /*var operador1 = 2;
+ var operador2 = 3;
+ var operado = "+";
+ var resultado = 0
+
+if (operado == "+"){
+    resultado = operador1 + operador2;
+}
+else if (operado == "*"){
+    resulatado = operador1 * operador2;
+}
+else if (operado == "-"){
+    resulatado = operador1 - operador2;
+}
+else if (operado == "/"){
+    resulatado = operador1 / operador2;
+}
+else if (operado == "^"){
+    resulatado = operador1 ^ operador2;
+}
+
+console.log(resultado)
+*/
+// 8. Implemente uma função que imprima todos os múltiplos de 5 menores que 20.
+
+/* var counter = 0;
+
+while (counter <= 10){
+        console.log(counter);
+    counter += 5;
+}
+
+for (let counter = 0; counter <= 20; counter++) {
+    if (counter % 5 == 0) {
+        console.log(counter)
+    }
+}
+*/
+//9. Implemente uma função que imprima a soma dos primeiros 100 números inteiros
+
+/*var soma = 0;
+
+for (let j = 0; j <= 100; j++) {
+    soma += j;
+}
+
+console.log(soma)
+
+//10.
+
+var fact = 1;
+for (let j = 1; j <= 3; j++) {
+    fact *= j;
+}
+
+console.log(fact);*/
+
+//11.
+
