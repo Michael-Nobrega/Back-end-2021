852434da9bd7fcd6f2dc606bc291f54932913074
Michael-Nobrega <2044720@student.uma.pt>
2021-04-12 17:29:59 +0100

    Create log.js

---
+++ ficha3/log.js
@@ -0,0 +1,20 @@
+var log = "hello Module!";
+
+var obj = {
+    messagem: "hello Message",
+    functionObj: function (info) {
+        return ("This fucntion outputs" + info);
+    }
+};
+
+var testFunction = function () {
+    return "Hello Function"
+}
+
+function ff() {
+    console.log("ff")
+}
+
+var test = "hello test";
+
+module.exports = Obj;
