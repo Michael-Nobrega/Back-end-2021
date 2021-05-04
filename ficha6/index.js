abbe48266cf6644140699c770d08830ba913252f
Michael-Nobrega <73909123+Michael-Nobrega@users.noreply.github.com>
2021-05-04 10:40:17 +0100

    ficha7

--- ficha6/index.js
+++ ficha6/index.js
@@ -1,4 +1,5 @@
 //importar o express
+const { request, response } = require('express');
 const express = require('express');
 const fs = require('fs');
 
@@ -10,21 +11,90 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//midware
+app.use(function (request, response, next) {
+  fs.appendFileSync('./log.txt', request.path + ", " + request.method + ", " + new Date() + "\r");
+  next()
+})
+
+
 //default get endpoint
 app.get('/', (request, response) => {
-    //var body = "<!DOCTYPE html><html><title>HTML Tutorial</title><body><h1>This is a heading</h1><p>This is a paragraph.</p></body></html>"
     var file = fs.readFileSync("./index.html", 'utf-8')
-
     var date = new Date();
-
     file = file.replace('{date}', date.toLocaleDateString());
-
     response.writeHead(200, {
           'Content-Lenght': Buffer.byteLength(file),
           'Content-Type': 'text/html'
       });
       response.end(file);
 });
+
+//@name: route parameter
+app.get('/user/:name', (request, response) => {
+  var file = fs.readFileSync("./index.html", 'utf-8')
+  var name = request.params.name;
+  file = file.replace('{name}', name);
+  response.writeHead(200, {
+        'Content-Lenght': Buffer.byteLength(file),
+        'Content-Type': 'text/html'
+    });
+    response.end(file);
+});
+
+//@name query parameter
+app.get('/user', (request, response) => {
+  var file = fs.readFileSync("./index.html", 'utf-8')
+  var name = request.query.name;
+  var profession = request.query.profession;
+  file = file.replace('{name}', name);
+  file = file.replace('{profession}', profession);
+  response.writeHead(200, {
+        'Content-Lenght': Buffer.byteLength(file),
+        'Content-Type': 'text/html'
+    });
+    response.end(file);
+});
+
+app.get('/log', (request, response) =>{
+  var file = fs.readFileSync("./log.txt", 'utf-8');
+  response.writeHead(200, {
+    'Content-Lenght': Buffer.byteLength(file),
+    'Content-Type': 'text/plain'
+  });
+  response.end(file);
+});
+
+
+
+app.get('/log.txt', (request, response) =>{ 
+  response.download('./log.txt', function(err){
+    if(err != undefined){
+      response.status(404);
+      response.end("Ocorreu um erro ao ler o ficheiro" + err.message)
+    }
+    else{
+      //Encontrou o ficheiro com sucesso
+    }
+  })
+});
+
+app.get('/clear', (request, response) =>{
+
+  fs.unlink('./log.txt', function name(err){
+    if(err){
+      response.status(404);
+      response.end("Ocorreu um erro ao apagar o ficheiro" + err.message)
+    }
+    else{
+        response.send("file deleted")
+    }
+  });
+
+  //fs.unlinkSync('./log.txt');
+  //response.send("File Deleted");
+});
+
 
 //metodo que arranca o servidor http e fica a escuta
 app.listen(port, () => {
